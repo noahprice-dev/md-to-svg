3109afbeea0a2463f8774763e271e2c9f73c1bc1
Noah Price <noah.price.237@gmail.com>
2026-04-06 17:46:48 -0400

    Add `RunState` statemachine struct type to handle proccessing glyphs in runs based on the style ranges provided.
    Update StyledInlineRange to store children of metadata containers
    
    Add `raw_text` function to LayoutInline to retrieve the text field of the type.
    
    Update `process_run to use new TSpanDefinition enum

--- src/layout.rs
+++ src/layout.rs
@@ -20,6 +20,18 @@ pub enum LayoutInline {
         title: Option<String>,
     },
 }
+// TODO - introduce separate type for nested variants to ensure we can only have valid text inside of Links.
+impl LayoutInline {
+    pub fn raw_text(&self) -> String {
+        match self {
+            LayoutInline::Text { text, .. } => text.clone(),
+            LayoutInline::HardBreak => "\n".to_string(),
+            LayoutInline::InlineLink { link_text, .. } => {
+                link_text.iter().map(|child| child.raw_text()).collect()
+            }
+        }
+    }
+}
 
 /// Shaped buffer, ready for SVG conversion
 #[derive(Debug)]
@@ -68,15 +80,194 @@ pub struct StyledInlineRange {
     segment_idx: usize,
     start_byte: usize,
     end_byte: usize,
+    child_idx: Option<usize>,
 }
 
 /// Complete definition of a Text Span SVG element.
-pub struct TspanDefinition {
-    text: String,
-    font_size: f32,
-    weight: Weight,
-    style: Style,
-    family: FamilyOwned,
+// TODO - introduce separate type for nested variants to ensure we can only have valid variants by type.
+#[derive(Clone)]
+pub enum TspanDefinition {
+    Text {
+        text: String,
+        font_size: f32,
+        weight: Weight,
+        style: Style,
+        family: FamilyOwned,
+    },
+    InlineLink {
+        text: Vec<TspanDefinition>,
+        url: String,
+        title: Option<String>,
+    },
+}
+
+#[derive(Default)]
+struct RunState {
+    current_text: String,
+    current_segment_idx: Option<usize>,
+    current_child_idx: Option<usize>,
+    /// Styled Link Text, without metadata
+    link_tspans: Vec<TspanDefinition>,
+}
+impl RunState {
+    // TODO: font_size belongs on LayoutInline::Text - relevant when implementing sub/superscript.
+    // TODO - reword this to be less robotic.
+    /// Move forward in the text state.
+    /// Compares the current state versus the inccoming state, optionally returning a TSpanDefinition if a segment change is detected.
+    /// In the case of a child segment changing within a segment, format and store the child range to be returned with other children on segment change.
+    /// 
+    fn advance(
+        &mut self,
+        new_char: &str,
+        new_segment_idx: usize,
+        new_child_idx: Option<usize>,
+        segments: &[LayoutInline],
+        font_size: f32,
+    ) -> Option<TspanDefinition> {
+        // todo rename
+        let result = match self.current_segment_idx {
+            // First glyph in the layout. Initialize State
+            None => None
+            ,
+            // * Same segment
+            Some(idx) if idx == new_segment_idx => {
+                // ? If we are not in a Link, then our current and new child will both be None.
+                // ? If we are in a link, one of these must be Some.
+                // * Same Segment, new child. We have a link with different styles of text within.
+                // * Flush current child text to `link_tspans`
+                if self.current_child_idx != new_child_idx { // ? Do we flip this?
+                    // ? Child boundary - flush text into self.link_tspans
+                    if self.current_child_idx.is_some() {
+                        // Get styling of current child:
+                        // ? We know that only InlineLink segments will have a child, however our type system doesn't yet clarify that,
+                        // ? so we need to do some weird pattern matching to fit this.
+                        match &segments[idx] {
+                            LayoutInline::InlineLink { link_text, .. } => {
+                                if let LayoutInline::Text {
+                                    text: _,
+                                    weight,
+                                    style,
+                                    family,
+                                } = &link_text[self.current_child_idx.expect(
+                                    "InlineLink variants off LayoutInline must be of type Text.",
+                                )] {
+                                    // * Update link spans for this segment
+                                    self.link_tspans.push(TspanDefinition::Text {
+                                        text: self.current_text.clone(),
+                                        font_size,
+                                        weight: weight.clone(),
+                                        style: style.clone(),
+                                        family: family.clone(),
+                                    });
+                                }
+                                // ? Partial flush. Reset our current text as this is not done unconditionally.
+                                self.current_text.clear();
+
+                                return None
+                            }
+                            // ? Unreachable, in theory - type system needs to back this.
+                            _ => unreachable!(
+                                "Child indices can only be found on Text variants of LayoutInline."
+                            ),
+                        }
+                    } else {
+                        // * No previous child
+                        None
+                    }
+                } else {
+                    // * Same segment, same child - accumulate and wait for the next change
+                    None
+                }
+            }
+            // * New Segment - emit prior segment
+            Some(_) => {
+                self.flush(segments, font_size)
+            }
+        };
+        // Unconditional state updates.
+        self.current_text.push_str(new_char);
+        self.current_child_idx = new_child_idx;
+        self.current_segment_idx = Some(new_segment_idx);
+
+        result
+    }
+
+    /// Emits the current accumulated state as a `TspanDefinition`
+    /// Clears `current_text` and `link_tspans` as part of emission.
+    /// Returns `None` if there is nothing to emit.
+    fn flush(&mut self, segments: &[LayoutInline], font_size: f32) -> Option<TspanDefinition> {
+        // Check if we have anything to process
+        // ? If current_text is not empty, we should be able to assume that `current_segment_idx` cannot be None, as it is a required field when we add any text.
+        // ? The inverse is not true. If `current_text` is empty, we cannot confidently say that `current_segment_idx` is None:
+        // ? the text is cleared during each child and segment transition.
+        // ? This logic holds if we are transitioning segments or forcing a final output.
+        let result: Option<TspanDefinition> =
+            if self.current_segment_idx.is_some() && !self.current_text.is_empty() {
+                // TODO evaluate if `clone` is neccessary here.
+                let segment = segments[self
+                    .current_segment_idx
+                    .expect("segment_idx must be Some when current_text is non-empty.")]
+                .clone();
+
+                match segment {
+                    LayoutInline::Text {
+                        text: _,
+                        weight,
+                        style,
+                        family,
+                    } => Some(TspanDefinition::Text {
+                        text: self.current_text.clone(),
+                        font_size,
+                        weight,
+                        style,
+                        family: family.clone(),
+                    }),
+                    LayoutInline::InlineLink {
+                        link_text: link_lyt_inlines,
+                        url,
+                        title,
+                    } => {
+                        // Crunch current text into a TspanDefinition.
+                        // ? The `link_text` vector of InlineLink cannot store another InlineLink due to behaviour defined outside of this function.
+                        // ? This is a common point of friction for me - it feels like we are defining behaviour that makes this invalid state impossible,
+                        // ? rather than relying on the type-system to guarantee it.
+                        if let LayoutInline::Text {
+                            text,
+                            weight,
+                            style,
+                            family,
+                        } = link_lyt_inlines[self.current_child_idx?].clone()
+                        {
+                            self.link_tspans.push(TspanDefinition::Text {
+                                text,
+                                font_size,
+                                weight,
+                                style,
+                                family: family.clone(),
+                            });
+                        }
+                        // emit
+                        Some(TspanDefinition::InlineLink {
+                            text: self.link_tspans.clone(),
+                            url,
+                            title,
+                        })
+                    }
+                    // This feels weak. but I don't know how to describe or enforce this.
+                    // The behaviour of `build_styled_inline_ranges` skips HardBreak in the counter and returns "\n".
+                    _ => unreachable!(
+                        "Non-text InlineStyles should not be parsed as a StyledInlineRange"
+                    ),
+                }
+            } else {
+                None
+            };
+        self.current_text.clear();
+        self.link_tspans.clear();
+
+        result
+    }
+    
 }
 
 pub fn process_layouts(layouts: Vec<LayoutItem>, cfg: &SvgConfig) -> Vec<String> {
@@ -137,23 +328,7 @@ fn process_layout_line(layout: &LayoutBlock, y_cursor: f32, cfg: &SvgConfig) -> 
     for (_idx, run) in layout.buffer.layout_runs().enumerate() {
         let baseline_y = y_cursor + run.line_y;
 
-        let full_text: &String = &layout
-            .segments
-            .iter()
-            .map(|seg| match seg {
-                LayoutInline::Text { text, .. } => text.clone(),
-                LayoutInline::HardBreak => "\n".to_string(),
-                // ? We are repeating ourself here, is there a world where we want to have an "Extract Raw Text" method on LayoutInline?
-                // ? I think unless we need to do this again, its fine.
-                LayoutInline::InlineLink { link_text, .. } => {
-                    link_text.iter().map(|txt| match txt {
-                        LayoutInline::Text { text, ..} => text.clone(),
-                        LayoutInline::HardBreak => "\n".to_string(),
-                        _ => unreachable!("InlineLink cannot have a nested link"),
-                    }).collect()
-                }, 
-            })
-            .collect();
+        let full_text: &String = &layout.segments.iter().map(|seg| seg.raw_text()).collect();
 
         let current_x = cfg.canvas_opts.padding.left + layout.indent_offset; // handle starting offset for the line of text.
 
@@ -206,7 +381,7 @@ fn process_run(
         }
 
         if !prefix_text.is_empty() {
-            tspans.push(TspanDefinition {
+            tspans.push(TspanDefinition::Text {
                 text: prefix_text,
                 font_size: font_size,
                 weight: Weight::NORMAL,
